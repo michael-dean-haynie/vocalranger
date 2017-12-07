aaaec495f7d7f99cd58d8280b4df67e532cca840
Michael <michael.dean.haynie@gmail.com>
2017-12-06 21:31:28 -0700

    Swaped out bootstrap v4 for foundation
    Bootstrap v4 didn't have all the kinks worked out yet (particularly the navbar) So I'm taking this opportunity to get familiar with foundation.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,22 +10,22 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
+    <!-- CSS -->
+    <link href="{{ asset('foundation/css/foundation.min.css') }}" rel="stylesheet"></link>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        {{-- Nav goes here --}}
+        @include('shared/navbar')
 
         @yield('content')
     </div>
 
     <!-- Scripts -->
+    <script src="{{ asset('foundation/js/vendor/jquery.js') }}"></script>
+    <script src="{{ asset('foundation/js/vendor/what-input.js') }}"></script>
+    <script src="{{ asset('foundation/js/vendor/foundation.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
